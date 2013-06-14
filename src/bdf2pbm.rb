3f29605b0b043aaf6ce6be2e85462b96519a8265
taro <taro@k03>
2013-06-14 11:57:08 +0900

    ???????

--- src/bdf2pbm.rb
+++ src/bdf2pbm.rb
@@ -9,15 +9,11 @@ if ARGV.length != 2
   exit 1
 end
 
-
 IO.read(ARGV[0]).scan(/STARTCHAR.*?ENDCHAR/m) do |s|
-
-  pbm =
-    "P4\n" +
-    s.match(/BBX\s+(\d+)/)[1] + " " + s.match(/BBX\s+\d+\s+(\d+)/)[1] + "\n" +
-    s.match(/BITMAP\s*\n(.*)ENDCHAR/m)[1].split("\n").inject("") { |sum,item| sum += [item].pack("H*") }
-  
-  filename = File.join(ARGV[1], sprintf("u%04x.pbm", s.match(/ENCODING\s+(\d+)/)[1]))
-  open(filename, "w") { |f| f << pbm }
-
+  File.open(File.join(ARGV[1], sprintf("u%04x.pbm", s.match(/ENCODING\s+(\d+)/)[1])), "wb:ascii-8bit") do |f|
+    f << "P4\n" +
+      s.match(/BBX\s+(\d+)/)[1] + " " +
+      s.match(/BBX\s+\d+\s+(\d+)/)[1] + "\n" +
+      s.match(/BITMAP\s*\n(.*)ENDCHAR/m)[1].split("\n").inject("") {|sum,item| sum += [item].pack("H*")}
+  end
 end
